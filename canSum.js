a4bfdb7c9b35d7eb8c79dd71e78beb5ddde3887a
LennyRemache <lenny.j.remache@gmail.com>
2022-09-12 21:11:47 -0400

    Implemented canSum memoized algorithm

---
+++ canSum.js
@@ -0,0 +1,17 @@
+function canSum(targetSum, numbers, memo = {}) {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true; // means we were able to find numbers that successfully reduce target sum to 0 exactly.
+  if (targetSum < 0) return false; // no more numbers available to reduce target to 0 exactly.
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    memo[remainder] = canSum(remainder, numbers, memo);
+    if (memo[remainder] === true) return true;
+  }
+  return false;
+}
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [5, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
