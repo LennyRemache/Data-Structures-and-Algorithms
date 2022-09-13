c43fdaf5962c2cb4cc690a7ad44815abc3c9d792
LennyRemache <lenny.j.remache@gmail.com>
2022-09-12 22:29:31 -0400

    Implemented bestSum memoized algorithm

---
+++ bestSum.js
@@ -0,0 +1,24 @@
+function bestSum(targetSum, numbers, memo = {}) {
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  let shortest = null;
+
+  for (let num of numbers) {
+    const remainder = targetSum - num;
+    const output = bestSum(remainder, numbers, memo);
+    if (output !== null) {
+      memo[targetSum] = [...output, num];
+      if (shortest === null || memo[targetSum].length < shortest.length) {
+        shortest = memo[targetSum];
+      }
+    }
+  }
+  return shortest;
+}
+
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(8, [1, 4, 5]));
+console.log(bestSum(100, [1, 2, 5, 25]));
